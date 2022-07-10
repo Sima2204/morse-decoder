d9c6d53a173bc3cc7e4b7c17b72c0c1a4209d5ec
Sima2204 <vlad.simuta@gmail.com>
2022-07-10 21:57:08 +0300

    fix: function working, all test passed

--- src/index.js
+++ src/index.js
@@ -41,11 +41,47 @@ function decode(expr) {
 
     const arr = Array. from(expr);
     const arrExpr = [];
-    
+
     for (let i = 0 ; i < arr.length; i += 10) {
         const part = arr.slice(i, i + 10);
-        res.push(part);
-    }
+        arrExpr.push(part);
+    } // arrays with 10 numbers made
+
+    const newArr = [];
+
+    arrExpr.forEach((element) => {
+        newString = '';
+        if (element[1] === '*') {
+            newArr.push('*');
+        } else {
+            for (i = 0; i <element.length; i = i + 2) {
+                if ((element[i] === '1') && (element[i+1] === '1')) {
+                    newString = newString + '-';
+                } else if ((element[i] === '1') && (element[i+1] === '0')) {
+                    newString = newString + '.';
+                }
+            }
+            newArr.push(newString);
+        }
+    }) // array with '-', '.' and '*' made
+
+    let str = '';
+    let keys = Object.keys(MORSE_TABLE);
+    let values = Object.values(MORSE_TABLE);
+
+    newArr.forEach((element) => {
+        if (element === '*') {
+            str = str + ' ';
+        } else {
+            for (let i = 0; i < keys.length; i++) {
+                if (element === keys[i]) {
+                    str = str + values[i];
+                }
+            }
+        }
+    }) // making decoding
+
+    return str; 
 
 }
 
